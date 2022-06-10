cb2b77d59c1ec0a3b4078ee933538aa281573c8d
Janek Paeßens <janekpaessens@t-online.de>
2022-06-10 12:54:17 +0200

    Some refactoring

--- app/src/main/java/de/fhac/newsflash/MainActivity.kt
+++ app/src/main/java/de/fhac/newsflash/MainActivity.kt
@@ -1,27 +1,19 @@
 package de.fhac.newsflash
 
 import android.content.Intent
-import android.graphics.RenderEffect
-import android.graphics.Shader
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.webkit.WebViewClient
-import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.navigation.NavigationBarView
 import de.fhac.newsflash.data.controller.NewsController
 import de.fhac.newsflash.data.models.News
 import de.fhac.newsflash.databinding.ActivityMainBinding
 import de.fhac.newsflash.databinding.BottomSheetBinding
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 
 
@@ -33,15 +25,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<FrameLayout>
     private var currentNews: News? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         bottomSheetBinding = binding.bottomSheet
         setContentView(binding.root)
 
-
         initNewsData()
-        initBottomSheetBehavior()
+        initBottomSheet()
         addCallbacks()
         addBottomNavigationCallback()
     }
@@ -61,7 +53,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
     private fun initNewsData() {
         runBlocking {
             newsList = NewsController.getNews()
@@ -71,7 +62,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun initBottomSheetBehavior() {
+    private fun initBottomSheet() {
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetBinding.bottomSheetRootLayout)
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
         bottomSheetBinding.webContent.webViewClient = WebViewClient()
@@ -79,42 +70,20 @@ class MainActivity : AppCompatActivity() {
 
     private fun addCallbacks() {
         bottomSheetBehavior.addBottomSheetCallback(NewsBottomSheetCallback(bottomSheetBinding))
+
         onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
-                when (bottomSheetBehavior.state) {
-                    BottomSheetBehavior.STATE_COLLAPSED -> bottomSheetBehavior.state =
-                        BottomSheetBehavior.STATE_HIDDEN
-                    BottomSheetBehavior.STATE_EXPANDED -> bottomSheetBehavior.state =
-                        BottomSheetBehavior.STATE_COLLAPSED
-                    else -> this@MainActivity.onBackPressed()
-                }
+                this@MainActivity.handleOnBackPressed()
             }
         })
-        bottomSheetBinding.btResizeNews.setOnClickListener {
-            when (bottomSheetBehavior.state) {
-                BottomSheetBehavior.STATE_COLLAPSED -> bottomSheetBehavior.state =
-                    BottomSheetBehavior.STATE_EXPANDED
-                BottomSheetBehavior.STATE_EXPANDED -> bottomSheetBehavior.state =
-                    BottomSheetBehavior.STATE_COLLAPSED
-            }
-        }
-        bottomSheetBinding.btShare.setOnClickListener {
-            val shareIntent = Intent(Intent.ACTION_SEND)
-            shareIntent.type = "text/plain"
-            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "NewsFlash")
-            var shareMessage = "\nLies dir diesen Artikel durch:\n\n${currentNews!!.url}"
-            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage)
-            startActivity(Intent.createChooser(shareIntent, "Welche App soll verw. werden?"))
-        }
-        bottomSheetBinding.btShowInBrowser.setOnClickListener {
-            val showInBrowserIntent = Intent(Intent.ACTION_VIEW)
-            showInBrowserIntent.data = Uri.parse(currentNews!!.url)
-            startActivity(showInBrowserIntent)
-        }
-        bottomSheetBinding.btSave.setOnClickListener {
-            NewsController.addFavorite(currentNews!!.id)
-            Toast.makeText(this@MainActivity, R.string.saved_to_favs, Toast.LENGTH_LONG).show()
-        }
+
+        bottomSheetBinding.btResizeNews.setOnClickListener { resizeNews() }
+
+        bottomSheetBinding.btShare.setOnClickListener { shareCurrentNews() }
+
+        bottomSheetBinding.btShowInBrowser.setOnClickListener { showCurrentNewsInBrowser() }
+
+        bottomSheetBinding.btSave.setOnClickListener { saveCurrentNewsToFavourite() }
     }
 
     fun showDetailedNews(news: News) {
@@ -131,5 +100,52 @@ class MainActivity : AppCompatActivity() {
 
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
     }
+
+
+    private fun handleOnBackPressed() {
+        when (bottomSheetBehavior.state) {
+            BottomSheetBehavior.STATE_COLLAPSED -> bottomSheetBehavior.state =
+                BottomSheetBehavior.STATE_HIDDEN
+            BottomSheetBehavior.STATE_EXPANDED -> bottomSheetBehavior.state =
+                BottomSheetBehavior.STATE_COLLAPSED
+            else -> this@MainActivity.onBackPressed()
+        }
+    }
+
+    private fun resizeNews() {
+        when (bottomSheetBehavior.state) {
+            BottomSheetBehavior.STATE_COLLAPSED -> bottomSheetBehavior.state =
+                BottomSheetBehavior.STATE_EXPANDED
+            BottomSheetBehavior.STATE_EXPANDED -> bottomSheetBehavior.state =
+                BottomSheetBehavior.STATE_COLLAPSED
+        }
+    }
+
+    private fun shareCurrentNews() {
+        currentNews?.let { news ->
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "NewsFlash")
+            var shareMessage = "\nLies dir diesen Artikel durch:\n\n${news.url}"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage)
+            startActivity(Intent.createChooser(shareIntent, "Welche App soll verwendet werden?"))
+        }
+    }
+
+    private fun showCurrentNewsInBrowser() {
+        currentNews?.let { news ->
+            val showInBrowserIntent = Intent(Intent.ACTION_VIEW)
+            showInBrowserIntent.data = Uri.parse(news.url)
+            startActivity(showInBrowserIntent)
+        }
+    }
+
+    private fun saveCurrentNewsToFavourite() {
+        currentNews?.let { news ->
+            NewsController.addFavorite(news.id)
+            Toast.makeText(this@MainActivity, R.string.saved_to_favs, Toast.LENGTH_LONG).show()
+        }
+    }
+
 
 }
